9773e76ab636b274439411f767f0714431f4b51d
Bryanth Briones <ayanbriones@gmail.com>
2023-04-08 22:29:46 +0300

    remove github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import image from '@astrojs/image'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
-  site: 'https://portfolio.bryanthbriones.com/',
-  base: '/public_html/portfolio-bryanthbriones-com',
+  // site: 'https://portfolio.bryanthbriones.com/',
+  // base: '/public_html/portfolio-bryanthbriones-com',
 })
