fef9289eb03f8b4a8ff5658a20a1d4a60de88bbb
Bryanth Briones <ayanbriones@gmail.com>
2023-04-08 21:49:45 +0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-import tailwind from "@astrojs/tailwind";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [react(), tailwind(), image()],
+  site: 'https://portfolio.bryanthbriones.com/',
+  base: '/public_html/portfolio-bryanthbriones-com',
+})
